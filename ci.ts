507cd0603de0324be067d5c7fef30fc4d40cceba
Tsiry Sandratraina <tsiry.sndr@fluentci.io>
2023-11-17 09:40:51 +0000

    use Dagger v0.9.3

--- ci.ts
+++ ci.ts
@@ -2,7 +2,7 @@ import {
   lintDebug,
   assembleDebug,
   debugTests,
-} from "https://pkg.fluentci.io/android_pipeline@v0.7.1/mod.ts";
+} from "https://pkg.fluentci.io/android_pipeline@v0.8.0/mod.ts";
 
 await lintDebug();
 await debugTests();
